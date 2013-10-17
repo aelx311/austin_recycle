fd3af1f66108a7129321c4f9fa2288f7152657fe
aelx311 <alex311@gmail.com>
2013-10-17 13:57:03 -0500

    added dialog to show material list

--- src/com/cs317m/austinrecycle/MainActivity.java
+++ src/com/cs317m/austinrecycle/MainActivity.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -15,30 +17,38 @@ import android.widget.ListView;
 public class MainActivity extends Activity {
 	
 	private EditText _materialList;
-	private ListView _materialListView;
-	private List<String> _materials;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-		_materials = new ArrayList<String>();
-		_materials.add("Oil");
-		_materials.add("Aluminum");
-		
 		_materialList = (EditText) this.findViewById(R.id.materials_editText);
 		_materialList.setInputType(DEFAULT_KEYS_DISABLE);
 		_materialList.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				MainActivity.this.setContentView(R.layout.material_list);
+				popMaterialDialog();
 			}
 		});
-		
-		_materialListView = (ListView) this.findViewById(R.id.material_list);
 	}
 
+	private void popMaterialDialog() {
+		final AlertDialog.Builder materialBuilder = new AlertDialog.Builder(this);
+		materialBuilder.setTitle("Please select materials");
+		
+		LayoutInflater inflater = this.getLayoutInflater();
+		final View materialListItemLayout = inflater.inflate(R.layout.material_list_item, null);
+		materialBuilder.setView(materialListItemLayout);
+		
+		final AlertDialog materialDialog = materialBuilder.create();
+		materialDialog.show();
+		
+		// create a list view
+		// create list to store materials
+		materialList = ()
+	}
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.

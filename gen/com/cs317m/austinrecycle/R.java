2d76959ce16e31b361963be7f619f41424c988d9
aelx311 <alex311@gmail.com>
2013-10-20 17:20:54 -0500

    updated R.java

--- gen/com/cs317m/austinrecycle/R.java
+++ gen/com/cs317m/austinrecycle/R.java
@@ -194,7 +194,6 @@ public final class R {
         public static final int location_list_item=0x7f030001;
         public static final int material_list_item=0x7f030002;
         public static final int material_list_view=0x7f030003;
-        public static final int material_listview=0x7f030004;
     }
     public static final class menu {
         public static final int main=0x7f090000;

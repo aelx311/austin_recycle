61db02aabec484298c1653dbd9c8c1f95574f04e
aelx311 <alex311@gmail.com>
2013-11-05 17:35:48 -0600

    changed logic for wifi and mobile checking

--- src/com/cs371m/austinrecycle/SplashScreenActivity.java
+++ src/com/cs371m/austinrecycle/SplashScreenActivity.java
@@ -16,12 +16,12 @@ public class SplashScreenActivity extends Activity {
 
 	private final static String TAG = "SplashScreenActivity";
 	private static int TIME_OUT = 3000;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_splash_screen);
-		
+
 		new Handler().postDelayed(new Runnable() {
 			@Override
 			public void run() {
@@ -29,17 +29,15 @@ public class SplashScreenActivity extends Activity {
 				ConnectivityManager conMan = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
 				State mobile = conMan.getNetworkInfo(ConnectivityManager.TYPE_MOBILE).getState();
 				State wifi = conMan.getNetworkInfo(ConnectivityManager.TYPE_WIFI).getState();
-				
+
 				Log.d(TAG, "mobile state: " + mobile.name());
 				Log.d(TAG, "wifi state: " + wifi.name());
-				
-				if(mobile.equals(State.DISCONNECTED) && wifi.equals(State.DISCONNECTED)
-//						&& mobile.equals(State.UNKNOWN) && wifi.equals(State.UNKNOWN)
-						) {
+
+				if(!(mobile.equals(State.CONNECTED) || wifi.equals(State.CONNECTED))) {
 					AlertDialog.Builder connectionDialogBuilder = new AlertDialog.Builder(SplashScreenActivity.this);
 					connectionDialogBuilder.setTitle("Connection error");
 					connectionDialogBuilder.setMessage(	"This app requires Internet connection.\n" +
-																				"Please make sure you are connected to the Internet.");
+							"Please make sure you are connected to the Internet.");
 					connectionDialogBuilder.setNegativeButton("Try again", new DialogInterface.OnClickListener() {
 						@Override
 						public void onClick(DialogInterface dialog, int which) {
@@ -55,7 +53,7 @@ public class SplashScreenActivity extends Activity {
 							SplashScreenActivity.this.recreate();
 						}
 					});
-					
+
 					AlertDialog connectionDialog = connectionDialogBuilder.create();
 					connectionDialog.show();
 				}
